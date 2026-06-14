73ffcb624da694ac8162bf24f2405fae76f9514b
Hokens240 <61212883+Hokens240@users.noreply.github.com>
2026-06-14 23:37:22 +0100

    Update

--- js/myapp.js
+++ js/myapp.js
@@ -85,10 +85,10 @@ function initializeMockUsers() {
             country: "United States of America", 
             pass: null, 
             accountBalance: "1,500.00", 
-            totalProfit: "0.00", 
+            totalProfit: "6621.00", 
             profitBalance: "0.00", 
             initialInvestment: "1,500.00", 
-            returnOnInvestment: "0.00",
+            returnOnInvestment: "4573.00",
             investments: []
         },
         { 
