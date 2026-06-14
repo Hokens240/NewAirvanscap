13634c9ef403d2e446fe9189b2c1728c073c3929
Hokens240 <61212883+Hokens240@users.noreply.github.com>
2026-06-14 19:39:15 +0100

    Update

--- js/myapp.js
+++ js/myapp.js
@@ -79,7 +79,7 @@ function initializeMockUsers() {
             investments: []
         },
         { 
-            email: "Josephconwell@gmail.com", 
+            email: "josephconwell@gmail.com", 
             firstName: "Joseph", 
             lastName: "Conwell", 
             country: "United States of America", 
